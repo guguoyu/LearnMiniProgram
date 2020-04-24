1b462b4bc956f03327c157c597e735c0a61d6c8d
顾国玉 <guguoyu@guguoyudeMacBook-Pro.local>
2020-04-24 18:03:24 +0800

    finsh learn radio

--- pages/radio/radio.js
+++ pages/radio/radio.js
@@ -1,70 +1,76 @@
 // pages/radio/radio.js
 Page({
 
-  radioChange(e){
-      console.log(e)
+  radioChange(e) {
+    //1.先获取radio的值
+    let sex = e.detail.value;
+    console.log(sex);
+    //2. 再对gender进行赋值
+    this.setData({
+      gender: sex === "male" ? "男" : "女"
+    });
   },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    gender: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
