84e74749cf178ef599b776cf6819a76a84d102da
guguoyu <guguoyu1@126.com>
2020-05-06 07:55:53 +0800

    完成自定义组件学习

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -4,35 +4,25 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    //要接受的数据名称
+    // aaa: {
+    //   // type 要接受的数据类型
+    //   type: String,
+    //   // value 默认值
+    //   value: ""
+    // }
 
+    items: {
+      type: Array,
+      value: []
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    items: [{
-      id: 0,
-      name: '首页',
-      isActive: true
-    },
-    {
-      id: 1,
-      name: '原创',
-      isActive: false
-    },
-    {
-      id: 2,
-      name: '分类',
-      isActive: false
-    },
-    {
-      id: 3,
-      name: '关于',
-      isActive: false
-    }
 
-    ]
 
   },
 
@@ -49,17 +39,22 @@ Component({
      * 
      */
     handleTap(e) {
-      console.log(e)
       // 获取点击的索引
       let choseIndex = e.currentTarget.dataset.index;
-      // 获取原数组
-      let { items } = this.data;
-      // 遍历数组
-      items.forEach((v, i) => i === choseIndex ? v.isActive = true : v.isActive = false);
 
-      this.setData({
-        items
-      })
+      // 点击时间触发的时候
+      // 触发父组件中的自定义事件 同时传递数据 给父组件
+      // this.triggerEvent("父组件自定义事件的名称",要传递的参数)
+      this.triggerEvent("itemChange",{choseIndex});
+      
+      // 获取原数组
+      // let { items } = this.data;
+      // // 遍历数组
+      // items.forEach((v, i) => i === choseIndex ? v.isActive = true : v.isActive = false);
+
+      // this.setData({
+      //   items
+      // })
     }
   }
 })
