c780474745b39f96c268196ab9469cbed29a2341
顾国玉 <guguoyu@guguoyudeMacBook-Pro.local>
2020-05-10 17:50:58 +0800

    learn pagelifecycle

--- pages/PageLifeCycle/PageLifeCycle.js
+++ pages/PageLifeCycle/PageLifeCycle.js
@@ -11,56 +11,63 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log("onLoad");
+    //一般用于异步请求
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    console.log("onReady");
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    console.log("onShow");
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide: function() {
+    console.log("onHide");
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
+  onUnload: function() {
+    console.log("onUnload");
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    console.log("onPullDownRefresh");
+    //页面的数据或者效果重新刷新
   },
 
   /**
    * 页面上拉触底事件的处理函数
+   * 需要有很多数据才能触发
    */
-  onReachBottom: function () {
-
+  onReachBottom: function() {
+    //上啦加载下一页数据
+    console.log("onReachBottom");
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function() {
+    console.log("onShareAppMessage");
+  },
+  onResize(){
+    console.log("onResize");
   }
 })
